59fb40d4101783c165f0b77990bcca6de663b9de
pccomponentshenry <rafael84barreiro@gmail.com>
2023-01-26 23:51:35 -0300

    create users

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-//                       _oo0oo_
-//                      o8888888o
-//                      88" . "88
-//                      (| -_- |)
-//                      0\  =  /0
-//                    ___/`---'\___
-//                  .' \\|     |// '.
-//                 / \\|||  :  |||// \
-//                / _||||| -:- |||||- \
-//               |   | \\\  -  /// |   |
-//               | \_|  ''\---/''  |_/ |
-//               \  .-\__  '-'  ___/-. /
-//             ___'. .'  /--.--\  `. .'___
-//          ."" '<  `.___\_<|>_/___.' >' "".
-//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-//         \  \ `_.   \_ __\ /__ _/   .-` /  /
-//     =====`-.____`.___ \_____/___.-`___.-'=====
-//                       `=---='
+
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+// MMMMMMMMWN0xdoooooooodKMMMMMMMMWX0kddoollo0W0ollllllllllloox0XWMMMMMMMMMMMMMMMMWXKkddooookNNkoooooooo0MMMW0olllllllxXMMMMMM
+// MMMMMMW0c'.          .xMMMMWKxc,..       .dWo               ..:dKWMMMMMMMMMMXkl;..       '0K,        cWMMWo        'OMMMMMM
+// MMMMMNd.             .xMMNk:.            .xWo                   .c0WMMMMMW0l.            ,0K,        cWMMWo        '0MMMMMM
+// MMMMWx.              .xMKc.              .xWo                     .lXMMMXl.              ,0K,        cWMMWo        'OMMMMMM
+// MMMMNl         .;::::l00,         .;cloooxKWo         .,;'.         :KWO,         ,clddddkNK;        cWMMWo        'OMMMMMM
+// MMMMX:       .oXWMMMMMK;        .lKWMMMMMMMMo         oWWN0o.        :d'        .xNMMMMMMMMK;        ;0000c        'OMMMMMM
+// MMMMK;       .dOOOOOOk;         .lxdxddxxkXWo         dMMMMWO.                  'oddddodoxXK;         ....         .OMMMMMM
+// MMMMK,                                   .kWo         dMMMMMWd.                          .OK,                      'OMMMMMM
+// MMMM0'                                   .kMo         dMMMMMMk.                          .OK,                      .OMMMMMM
+// MMMM0'                          .'''''''':OWo         dMMMMMNl                  .'''''''':0K;        .''''.        .OMMMMMM
+// MMMMO.       .;c:::::c:.        ,ONNNNNNNWWWo         dMMMMXl.        .         ,0WWNNNNNNWK;        cNNNNo        .OMMMMMM
+// MMMMO.       '0MMMMMMMWx.        .lkKXXNNNWWo         dMXxc.         c0l         ,oOKKXXXXWK;        cWMMWo        .OMMMMMM
+// MMMMk.       '0MMMMMMMMWx.          ...'';OMo         dMx.         .oNMNd.          .....:KK;        cWMMWo        .OMMMMMM
+// MMMMk.       '0MMMMMMMMMWKc.             .kMo         dMd.       .;OWMMMWO:.             ,0K;        cWMMWo        'OMMMMMM
+// MMMMk.       '0MMMMMMMMMMMWKd;.          .kWo        .dMd.     'cOWMMMMMMMWKd:..         ,0K;        cWMMWd        '0MMMMMM
+// MMMMO;.......cKMMMMMMMMMMMMMMWKOdlc;'....;OWx'.......,kMk'';cdOXMMMMMMMMMMMMMWXOxlc:,'...cKXc........dWMMWx'.......:KMMMMMM
+// MMMMWNXXXXXXXNWMMMMMMMMMMMMMMMMMMMMWNNXXXNWMWXXXXXXXXXWMNXNWMMMMMMMMMMMMMMMMMMMMMMMWWNNXXNWWNXXXXXXXXWMMMMWXXXXXXXXNWMMMMMM
+// MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
