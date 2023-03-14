26ae3958db7ce1777afb37faecadad4088c74111
pccomponentshenry <rafael84barreiro@gmail.com>
2023-03-14 01:12:03 -0300

    index

--- index.js
+++ index.js
@@ -19,13 +19,13 @@
 // MMMMWNXXXXXXXNWMMMMMMMMMMMMMMMMMMMMWNNXXXNWMWXXXXXXXXXWMNXNWMMMMMMMMMMMMMMMMMMMMMMMWWNNXXNWWNXXXXXXXXWMMMMWXXXXXXXXNWMMMMMM
 // MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+import { listen } from './src/app.js';
+import { conn } from './src/db.js';
 const port = process.env.PORT || 3001;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
+  listen(3001, () => {
     console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
 });
